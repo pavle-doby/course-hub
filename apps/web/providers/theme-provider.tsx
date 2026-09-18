49913739c521d028eefac27a3c64985816fd79b3
Pavle Marinkovic <iampavle.sync@gmail.com>
2026-09-18 16:05:18 +0200

    Set dark as default theme

--- apps/web/providers/theme-provider.tsx
+++ apps/web/providers/theme-provider.tsx
@@ -9,7 +9,7 @@ function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextT
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
       {...props}
